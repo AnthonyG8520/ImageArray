02f045960f1c4bf8216d07e9b8bbb33706817339
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-06-14 12:33:11 -0500

    first commit

---
+++ src/main/java/com/example/imagearray/ImageArrayApplication.java
@@ -0,0 +1,13 @@
+package com.example.imagearray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageArrayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageArrayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imagearray/ImageArrayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imagearray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageArrayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
