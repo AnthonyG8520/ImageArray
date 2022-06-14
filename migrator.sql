81191480b0bfe840ae25a031eb3fd7b25f4ed51e
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-06-14 13:04:11 -0500

    added partial user model

---
+++ migrator.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS image_array_db;
+CREATE DATABASE IF NOT EXISTS image_array_db;
+USE image_array_db;
---
+++ src/main/java/com/example/imagearray/models/User.java
@@ -0,0 +1,18 @@
+package com.example.imagearray.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String email;
+
+}
