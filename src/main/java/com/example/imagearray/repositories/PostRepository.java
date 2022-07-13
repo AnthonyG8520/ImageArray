5783c63bce124810445d22dcc4d03123c6ecee32
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-07-13 03:14:42 -0500

    added custom query to pull only followed users and sort posts by date

--- src/main/java/com/example/imagearray/repositories/PostRepository.java
+++ src/main/java/com/example/imagearray/repositories/PostRepository.java
@@ -2,6 +2,11 @@ package com.example.imagearray.repositories;
 
 import com.example.imagearray.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query(value= "SELECT * FROM POST WHERE user_id IN (?1) ORDER BY date DESC", nativeQuery = true)
+    List<Post>getFollowedUserPostByTime(String ids);
 }
