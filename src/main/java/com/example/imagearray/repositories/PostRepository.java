ee6fc6bdd1d316166b410c0123ff9df0c54a6856
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-08-26 00:16:08 -0500

    added new query for getting users posts and sorting by time

--- src/main/java/com/example/imagearray/repositories/PostRepository.java
+++ src/main/java/com/example/imagearray/repositories/PostRepository.java
@@ -14,4 +14,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @Query(value = "SELECT * FROM post ORDER BY date DESC",nativeQuery = true)
     List<Post>findAllOrderByDate();
+
+    @Query(value = "SELECT * FROM post WHERE user_id = ? ORDER BY date DESC", nativeQuery = true)
+    List<Post>getUsersPostsByTime(Long id);
 }
