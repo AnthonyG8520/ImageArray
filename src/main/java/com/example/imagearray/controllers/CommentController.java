e120b7abc425f9920dc0895a603218ee0a405977
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2022-09-06 23:31:42 -0500

    new method opened in comment controller for saving comnments (not done yet)

--- src/main/java/com/example/imagearray/controllers/CommentController.java
+++ src/main/java/com/example/imagearray/controllers/CommentController.java
@@ -1,7 +1,10 @@
 package com.example.imagearray.controllers;
 
+import com.example.imagearray.models.Comment;
 import com.example.imagearray.repositories.CommentRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CommentController {
@@ -11,5 +14,12 @@ public class CommentController {
         this.commentDao = commentDao;
     }
 
+    @PostMapping("/comment/create")
+    public String saveComment(@ModelAttribute Comment comment){
+
+
+        commentDao.save(comment);
+        return "redirect:/post/" + postid;
+    }
 
 }
