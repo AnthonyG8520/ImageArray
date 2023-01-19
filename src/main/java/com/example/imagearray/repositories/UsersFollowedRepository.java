712c07bbca69b8bfb449b477b3132fd1e7994558
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-01-19 00:35:53 -0600

    deleted old method for getting followers and then counting / replaced with custom count query

--- src/main/java/com/example/imagearray/repositories/UsersFollowedRepository.java
+++ src/main/java/com/example/imagearray/repositories/UsersFollowedRepository.java
@@ -3,11 +3,13 @@ package com.example.imagearray.repositories;
 import com.example.imagearray.models.User;
 import com.example.imagearray.models.UsersFollowed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface UsersFollowedRepository extends JpaRepository<UsersFollowed, Long> {
     UsersFollowed getByFollowedUserAndUser(User followedUser, User User);
 
-    List<UsersFollowed> getByFollowedUser(User followedUser);
+    @Query(value = "SELECT COUNT(*) FROM users_followed WHERE followed_user = ?", nativeQuery = true)
+    int getFollowersCount(long userId);
 }
