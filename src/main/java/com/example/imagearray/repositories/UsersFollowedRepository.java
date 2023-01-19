233002953fcc689d6f4c42ebbf283f45f74222b1
AnthonyG8520 <anthony.j.gonzales22@gmail.com>
2023-01-19 00:12:24 -0600

    added new query to usersFollowed repo in order to query all lines with specific user being followed

--- src/main/java/com/example/imagearray/repositories/UsersFollowedRepository.java
+++ src/main/java/com/example/imagearray/repositories/UsersFollowedRepository.java
@@ -4,6 +4,10 @@ import com.example.imagearray.models.User;
 import com.example.imagearray.models.UsersFollowed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsersFollowedRepository extends JpaRepository<UsersFollowed, Long> {
     UsersFollowed getByFollowedUserAndUser(User followedUser, User User);
+
+    List<UsersFollowed> getByFollowedUser(User followedUser);
 }
